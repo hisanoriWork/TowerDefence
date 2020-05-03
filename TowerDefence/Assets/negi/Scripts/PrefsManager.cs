eafcbbb8832975d36ee1214f21a45876eff489d6
welshonion <45817850+welshonion@users.noreply.github.com>
2020-05-04 02:00:16 +0900

    Create PrefsManager

---
+++ TowerDefence/Assets/negi/Scripts/PrefsManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*[Serializable]*/
+
+
+public class Formation
+{
+    public int[] gridinfo = new int[120];
+    public int shiptype;
+}
+
+
+public class PrefsManager
+{ 
+    Formation formation = new Formation();
+
+
+    public Formation getFormation()
+    {
+
+        string json = PlayerPrefs.GetString("formation","oyo");
+        Formation formation = JsonUtility.FromJson<Formation>(json);
+
+        return formation;
+    }
+
+
+    public void setFormation(int[] gridinfo,int shiptype)
+    {
+        formation.gridinfo = gridinfo;
+        formation.shiptype = shiptype;
+
+        string json = JsonUtility.ToJson(formation);
+
+        Debug.Log("json:"+json);
+
+        PlayerPrefs.SetString("formation",json);
+
+        return;
+    }
+
+
+    public void delete()
+    {
+        PlayerPrefs.DeleteAll();
+
+        return;
+    }
+
+
+}
---
+++ TowerDefence/Assets/negi/Scripts/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    //編成データを読み書きするときはPrefManager.csにあります
+
+    //編成データ読み書き用
+    PrefsManager prefs = new PrefsManager();
+    //データ
+    Formation formation = new Formation();//マス目部分int[] gridinfo = new int[120] ,船部分 int shiptype;
+
+    public void debugship()
+    {
+        //編成データ書き込み例
+        for (int i = 0; i < 120; i++)
+        { 
+            formation.gridinfo[i] = Random.Range(100, 200);
+        }
+        formation.shiptype = 114514;
+        prefs.setFormation(formation.gridinfo, formation.shiptype);
+
+
+        //編成データ読み込み、使用例
+        formation = prefs.getFormation();
+        for (int i = 0; i < 120; i++)
+        {
+            Debug.Log(formation.gridinfo[i]);
+        }
+        Debug.Log(formation.shiptype);
+
+
+    }
+
+}
