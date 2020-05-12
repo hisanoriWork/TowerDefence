3b3bebf56bf95eae58616f6698f622e2e6286d89
skip3skip3 <51410685+skip3skip3@users.noreply.github.com>
2020-05-12 17:44:14 +0900

    CannonPngnを作成中

---
+++ TowerDefence/Assets/Data/MissileData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/MissileData")]
+public class MissileData : ScriptableObject//飛び道具データ
+{
+    [Range(0, 90)] public float angle;//角度
+    [Range(0, 30)] public float deviation;//偏差
+    [Range(1, 10)] public float speed;//速度
+    [Range(0, 10)] public float gravity;//重力
+}
