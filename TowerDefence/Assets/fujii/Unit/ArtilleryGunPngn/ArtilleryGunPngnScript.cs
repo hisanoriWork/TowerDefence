676289e5344935c2ebdab33d11b1aabd038e3f1d
skip3skip3 <51410685+skip3skip3@users.noreply.github.com>
2020-05-18 23:05:32 +0900

    feat：連射するようにした

--- TowerDefence/Assets/fujii/Unit/ArtilleryGunPngn/ArtilleryGunPngnScript.cs
+++ TowerDefence/Assets/fujii/Unit/ArtilleryGunPngn/ArtilleryGunPngnScript.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+using MyLibrary;
 public class ArtilleryGunPngnScript : MonoBehaviour
 {
     /*****public field*****/
     public BulletPool pool;
     public Transform attackTransform;
+    public float time;
+    public int num;
     /*****Menobehaviour method*****/
     /*****public method*****/
     public void Attack(int power)
     {
-        pool.Pop(attackTransform.position);
+        Shot(num);
+    }
+
+    public void Shot(int num)
+    {
+        if (num > 0)
+        {
+            pool.Pop(attackTransform.position);
+            StartCoroutine(Utility.WaitForSecond(time, () =>
+            {
+                Shot(--num);
+            }));
+        }
     }
 }
