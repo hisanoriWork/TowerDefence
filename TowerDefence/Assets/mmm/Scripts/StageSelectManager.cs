04294f885df29be7e463c9dfc1eff66e3ecad2c4
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-19 07:27:33 +0900

    feat: StageSelectManagerとStageItemListenerの作成

---
+++ TowerDefence/Assets/mmm/Scripts/StageSelectManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectManager : MonoBehaviour
+{
+    /*****public field*****/
+    public int stageNum = 1;
+
+    void Awake()
+    {
+    }
+
+    public void ChangeSelectStage(int selectStageNum)
+    {
+        stageNum = selectStageNum;
+        Debug.Log("ステージ" +selectStageNum + "が選ばれました");
+        //TODO: MissionDetailWindowを変更する
+    }
+}
