16cefe7122ad3f5a2d19d4bd6d57e498fb695f3b
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-19 16:46:20 +0900

    feat: スライダーで編成を変更可能に

---
+++ TowerDefence/Assets/mmm/Scripts/SlotSliderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotSliderController : MonoBehaviour
+{
+    public int ownFormationNum = 1;
+    public Slider slotSlider;
+    public Text slotText;
+
+
+    void Start()
+    {
+        // TODO: ownFormationNumの値を繁栄させる
+        slotSlider.value = ownFormationNum;
+    }
+
+    public void OnValueChanged()
+    {
+        ownFormationNum = (int)slotSlider.value;
+        slotText.text = "編成 " + ownFormationNum;
+    }
+}
