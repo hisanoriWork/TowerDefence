14dda9e6f86144cde855098f59d6e7102ab5934c
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-21 12:04:15 +0900

    feat: 動的にScrollViewの中身を描写

--- TowerDefence/Assets/mmm/Scripts/StageItemListener.cs
+++ TowerDefence/Assets/mmm/Scripts/StageItemListener.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class StageItemListener : MonoBehaviour
 {
     public int stageNum = 0;
-    public StageSelectManager stageSelectManager;
+    private StageSelectManager stageSelectManager;
 
+    void Start()
+    {
+        stageSelectManager = GameObject.Find("StageSelectManager").GetComponent<StageSelectManager>();
+    }
     public void ClickOnListener()
     {
         if (stageSelectManager != null)
         {
             stageSelectManager.ChangeSelectStage(stageNum);
             Debug.Log("ステージ番号:" + stageNum);
-        } else
+        }
+        else
         {
             Debug.Log("stageSelectManagerのインスタンスが存在しません");
         }
