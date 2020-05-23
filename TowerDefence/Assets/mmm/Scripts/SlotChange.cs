c1115c700974b9abc84f67a53a1cf650a3a64673
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-24 08:18:44 +0900

    feat: 編成ボタンの設置

---
+++ TowerDefence/Assets/mmm/Scripts/SlotChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotChange : MonoBehaviour
+{
+    public int ownFormationNum = 1;
+    public GameObject[] slotBtns;
+
+    public void A(GameObject test)
+    {
+        Debug.Log("aaaa");
+        for (int i = 0; i < slotBtns.Length; i++)
+        {
+            slotBtns[i].SetActive(true);
+            if (slotBtns[i] == test)
+            {
+                //TODO:
+                test.SetActive(false);
+            }
+        }
+    }
+}
