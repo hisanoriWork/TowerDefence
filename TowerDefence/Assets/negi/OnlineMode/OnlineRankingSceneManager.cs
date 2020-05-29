2a77187f03118a9d0af22a946eb3e01c6716ff06
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-29 15:11:02 +0900

    chore: OnlineRankingAreaの調整

--- TowerDefence/Assets/negi/OnlineMode/OnlineRankingSceneManager.cs
+++ TowerDefence/Assets/negi/OnlineMode/OnlineRankingSceneManager.cs
@@ -6,24 +6,32 @@ using UnityEngine.UI;
 
 public class OnlineRankingSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject rankingContainer;
+    public GameObject rankingLayout;
+
+    private NCMBDatabase database = new NCMBDatabase();
     void Start()
     {
         //BGMManager.instance.SetVolume(1);
         //BGMManager.instance.Play("タイトル");
         BGMManager.instance.Play("タイトル");
+
+        // TODO: 上位ランキングのデータFetch
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void LoadOnlineEntranceScene()
     {
         SEManager.instance.Play("シーン遷移");
         SceneManager.LoadScene("OnlineEntranceScene");
     }
 
+    private void InflateItems(List<StageData> topStageDataList)
+    {
+        // Inflate
+        foreach (StageData data in topStageDataList)
+        {
+            // TODO: データを反省させる
+
+            Instantiate(rankingLayout, rankingContainer.transform);
+        }
+    }
 }
